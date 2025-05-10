0283ccd6af0425eadf0e0e98b4e0d8479b6e9094
l3aonti <l3aontirf@mail.ru>
2025-05-11 01:00:36 +0300

    create void main

---
+++ Bogdanov_project_LR4.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+void inputA(int& a) {
+
+}
+
+void inputB(int& b) {
+
+}
+
+void calculateArea(int a, int b) {
+
+}
+
+void calculatePerimeter(int a, int b) {
+
+}
+
+int main() {
+    int a = 0, b = 0;
+    int choice;
+
+    while (true) {
+        cout << "\nМеню:\n";
+        cout << "1. Ввести положительное число A\n";
+        cout << "2. Ввести положительное число B\n";
+        cout << "3. Найти площадь прямоугольника\n";
+        cout << "4. Найти периметр прямоугольника\n";
+        cout << "0. Выход\n";
+        cout << "Выберите пункт: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                inputA(a);
+                break;
+            case 2:
+                inputB(b);
+                break;
+            case 3:
+                calculateArea(a, b);
+                break;
+            case 4:
+                calculatePerimeter(a, b);
+                break;
+            case 0:
+                return 0;
+            default:
+                cout << "Неверный выбор! Попробуйте снова.\n";
+        }
+    }
+}
